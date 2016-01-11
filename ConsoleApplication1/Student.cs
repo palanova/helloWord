79be2be0e98171f1f404eb7f9dbd6d91835275fb
palanova <palanova-lera@mail.ru>
2016-01-11 14:18:14 +0300

    CreateClasses

---
+++ ConsoleApplication1/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Student
+    {
+        public int StudentId { get; set; }
+        public string SName { get; set; }
+        public string Subject { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Teacher
+    {
+        public int TeacherID { get; set; }
+        public string TName { get; set; }
+        public string Subject { get; set; }
+        
+        
+        
+    }
+}
