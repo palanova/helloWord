bc8e4cc360c56bfa590866cfba2f6ecc5b2a7c2a
mmonastyrskaya <monasmaria@gmail.com>
2016-01-11 14:32:44 +0300

    Migrations

--- ConsoleApplication1/Context.cs
+++ ConsoleApplication1/Context.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.Entity;
 
 namespace ConsoleApplication1
 {
-    class Program
+    class Context:DbContext
     {
-        static void Main(string[] args)
-        {
+        public Context():base("University")
+        {            
         }
     }
 }
